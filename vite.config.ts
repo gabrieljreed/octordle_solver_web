42ee4f37e21e317853089ea047651a73f6d37fc5
Gabriel Reed <gabrieljreed@gmail.com>
2026-06-30 16:18:17 -0700

    Update base

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/octordle_solver/",
+  base: "/octordle_solver_web/",
   build: {
     outDir: "../docs",
     emptyOutDir: true,
