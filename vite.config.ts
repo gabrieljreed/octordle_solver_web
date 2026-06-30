72257eea51d36c3d27c4b05c3c752b1421fc51e6
Gabriel Reed <gabrieljreed@gmail.com>
2026-06-30 16:36:16 -0700

    Move build to "docs" folder

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   base: "/octordle_solver_web/",
   build: {
-    outDir: "build",
+    outDir: "docs",
     emptyOutDir: true,
   },
   worker: {
